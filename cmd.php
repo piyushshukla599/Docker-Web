13042448eaad69df8586b8ea99e53936c8ee4c88
Ankur Acharjee <29690304+Cyb3rG0dzilla@users.noreply.github.com>
2022-05-19 22:11:59 +0530

    Add files via upload

---
+++ cmd.php
@@ -0,0 +1,60 @@
+<?php
+$cmd = $_GET['x'];
+if (isset($_GET['y'])){
+    $y = $_GET['y']; 
+}
+if (isset($_GET['z'])){
+    $z = $_GET['z'];
+}
+
+switch($cmd){
+    case "list":
+        echo "<br />";
+        echo "<pre>";
+        $cmd2 = 'sudo docker ps';
+        $content = system($cmd2, $ret);
+        echo "</pre>";
+        echo "<br />";
+        break;
+    case "images":
+        echo "<br />";
+        echo "<pre>";
+        $cmd2 = 'sudo docker images';
+        $content = system($cmd2, $ret);
+        echo "</pre>";
+        echo "<br />";
+        break;
+    case "run":
+        echo "<br />";
+        echo "<pre>";
+        $cmd2 = "sudo docker run -dit $y $z";
+        $content = system($cmd2, $ret);
+        echo "</pre>";
+        echo "<br />";
+        break;
+    case "exec":
+        echo "<br />";
+        echo "<pre>";
+        $cmd2 = "sudo docker exec -d $y $z";
+        $content = system($cmd2, $ret);
+        echo "</pre>";
+        echo "<br />";
+        break;
+    case "rem":
+        echo "<br />";
+        echo "<pre>";
+        $cmd2 = "sudo docker rm $y --force";
+        $content = system($cmd2, $ret);
+        echo "</pre>";
+        echo "<br />";
+        break;
+    case "remi":
+        echo "<br />";
+        echo "<pre>";
+        $cmd2 = "sudo docker rmi $y --force";
+        $content = system($cmd2, $ret);
+        echo "</pre>";
+        echo "<br />";
+        break;
+}
+?>
